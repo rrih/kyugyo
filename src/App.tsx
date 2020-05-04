46c86553085a5b54280594194c37eda9e2ba1964
ryohek <ryohei@weseek.co.jp>
2020-05-05 06:30:20 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import axios from "axios";
+// import { render } from 'react-dom';
+// import { KyugyoType } from './models/interfaces';
+// import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// type Kyugyos = {
+//   kyugyo: KyugyoType[];
+// }
+
+// export const KyugyoContext = React.createContext<Partial<ContextProps>>({});
+const apiUrl = 'http://localhost:8000/api/kyugyos';
+
+class App extends React.Component {
+  constructor() {
+    super({});
+    this.state = {
+      kyugyos: [],
+    };
+  }
+  // 開発環境であるかどうか
+  // TODO ここの切り替えどうにかする
+  // const isDevEnv = true;
+  // const devUrl = "http://localhost:8080/";
+  // const prodUrl = "https://kyugyo-back.herokuapp.com/";
+  // const apiUrl = isDevEnv ? `${devUrl}/api/kyugyos` : `${prodUrl}/api/kyugyos`;
+
+  getKyugyos = async () => {
+    axios.get(apiUrl)
+      .then((response) => {
+        this.setState({
+          kyugyos: response.data
+        })
+        console.log('以下、this.state');
+        console.log(this.state);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+
+  render() {
+    return (
+      <div>
+          <button onClick={this.getKyugyos} className="btn btn-primary">get kyugyos</button>
+          <div>
+            {/* {this.state.kyugyos} */}
+          </div>
+      </div>
+    );
+  }  
 }
 
 export default App;
---
+++ src/models/interfaces.tsx
@@ -0,0 +1,5 @@
+export type KyugyoType = {
+    id: string;
+    storeName: string;
+    isClosed: boolean;
+}
