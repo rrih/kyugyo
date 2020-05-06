eb5f987de7db056a9d1996ec2f3c9c2e0291ddae
ryohek <ryohei@weseek.co.jp>
2020-05-06 19:15:14 +0900

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
-import './index.scss';
-// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
