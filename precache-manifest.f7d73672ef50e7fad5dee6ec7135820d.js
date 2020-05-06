ba0bb365ca744354089f6a41a1778654bb4e6bf7
ryohek <ryohei@weseek.co.jp>
2020-05-06 20:25:55 +0900

    Updates

--- precache-manifest.f7d73672ef50e7fad5dee6ec7135820d.js
+++ precache-manifest.f7d73672ef50e7fad5dee6ec7135820d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f6e17af015e444df0e13f6b291ef84e2",
+    "revision": "6f8db3180f7630518d00479a7cc4790a",
     "url": "/kyugyo-front/index.html"
   },
   {
-    "revision": "def47aae3067e763a727",
+    "revision": "d7b16cb25b970295bf40",
     "url": "/kyugyo-front/static/css/main.f7ba274a.chunk.css"
   },
   {
-    "revision": "9321be08cb720ed66f55",
-    "url": "/kyugyo-front/static/js/2.4dbf9523.chunk.js"
+    "revision": "b179b27e2806de7015cc",
+    "url": "/kyugyo-front/static/js/2.935a964e.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/kyugyo-front/static/js/2.4dbf9523.chunk.js.LICENSE.txt"
+    "url": "/kyugyo-front/static/js/2.935a964e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "def47aae3067e763a727",
-    "url": "/kyugyo-front/static/js/main.20f234ce.chunk.js"
+    "revision": "d7b16cb25b970295bf40",
+    "url": "/kyugyo-front/static/js/main.d6e60566.chunk.js"
   },
   {
     "revision": "a32c110d8ea846c32417",
