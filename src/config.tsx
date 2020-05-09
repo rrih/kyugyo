e8ecb739be5c3ade74cbb3537ee095224e4c36ac
ryohek <ryohei@weseek.co.jp>
2020-05-09 12:10:08 +0900

    update

--- src/config.tsx
+++ src/config.tsx
@@ -2,6 +2,6 @@ const localUrl = 'http://localhost:8000';
 const prodUrl = 'https://kyugyo-back.herokuapp.com';
 // const tmpUrl = process.env.NODE_ENV === 'development' ? localUrl : prodUrl;
 
-const apiUrl = `${localUrl}/api/kyugyos`;
-// const apiUrl = `${prodUrl}/api/kyugyos`;
+// const apiUrl = `${localUrl}/api/kyugyos`;
+const apiUrl = `${prodUrl}/api/kyugyos`;
 export default apiUrl;
