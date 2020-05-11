3337c907c61e3e2d305714fd074f4f4646621f99
ryohek <ryohei@weseek.co.jp>
2020-05-11 23:33:09 +0900

    update

--- src/config.tsx
+++ src/config.tsx
@@ -2,8 +2,8 @@ const localUrl = 'http://localhost:8000';
 const prodUrl = 'https://kyugyo-back.herokuapp.com';
 // const tmpUrl = process.env.NODE_ENV === 'development' ? localUrl : prodUrl;
 
-const apiUrl = `${localUrl}/api/kyugyos`;
-// const apiUrl = `${prodUrl}/api/kyugyos`;
+// const apiUrl = `${localUrl}/api/kyugyos`;
+const apiUrl = `${prodUrl}/api/kyugyos`;
 export default apiUrl;
 
 export const commentUrl = `${localUrl}/api/kyugyos/comments`;
